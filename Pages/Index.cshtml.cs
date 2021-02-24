7d1f7b95e5b8f7b1874fd17bf7a491dcc4ae35a1
morsesolutions <morsesolutions@outlook.com>
2021-02-24 15:22:31 -0500

    testing hooks 3

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -17,7 +17,7 @@ namespace SampleApp.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private IConfiguration _config;
-        private string secretagainAPIKey = "AIzaSyCuEKyaBjjAkbWkhwhrs9Sg1574nOs8QRM";
+        private string secretGoogleAPIKey = "AIzaSyCuEKyaBjjAkbWkhwhrs9Sg1574nOs8QRM";
 
         public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
         {
