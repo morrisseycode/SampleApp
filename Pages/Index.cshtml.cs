f601ddb305d095faee208af86e796c99c7bec2e1
morsesolutions <morsesolutions@outlook.com>
2021-02-24 15:58:55 -0500

    testing hooks 3

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -17,7 +17,7 @@ namespace SampleApp.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private IConfiguration _config;
-        private string secret5APIKey = "AIzaSyCuEKyaBjjAkbWkhwhrs9Sg1574nOs8QRM";
+        private string secret6APIKey = "AIzaSyCuEKyaBjjAkbWkhwhrs9Sg1574nOs8QRM";
 
         public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
         {
